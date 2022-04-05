e4ab4551b7b45e3bef53cf7142e66d0b001e9e2a
Alexis Metton <alexis.metton@gmail.com>
2022-04-05 08:41:51 +0200

    section1 debut

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,16 @@
+function transitionAcceuilDroite(){
+    document.getElementById('imageHotel').style.display = "none";
+    document.getElementById('imageSafari').style.display = "block";
+    } 
+function transitionAcceuilDroite2(){
+    document.getElementById('imageSafari').style.display = "none";
+    document.getElementById('imageCroisiere').style.display = "block";
+}
+function transitionAcceuilGauche2(){
+    document.getElementById('imageHotel').style.display = "block";
+    document.getElementById('imageSafari').style.display = "none";
+}
+function transitionAcceuilGauche3(){
+    document.getElementById('imageSafari').style.display = "block";
+    document.getElementById('imageCroisiere').style.display = "none";
+}
